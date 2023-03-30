5d85252da1990df076c5913a42bbac3e7a114bfe
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-30 11:11:56 +0300

    Add some books and form

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,4 +1,36 @@
+import PropTypes from 'prop-types';
+import './styles/Books.css';
 const Books = () => (
-    <div>Books</div>
+    <div className="books">
+    <ul className="booklist">
+      <li className="item">
+        <Book title="The Vicar of Wakefield" author="Oliver Goldsmith" />
+      </li>
+      <li className="item">
+        <Book title="The Decline and Fall of the Roman Empire:" author="Edward Gibbon" />
+      </li>
+      <li className="item">
+        <Book title="The Merchant of Venice" author="Shakespeare" />
+      </li>
+    </ul>
+    <form className="form">
+      <input type="text" placeholder="Book title" className="input"/>
+      <input type="text" placeholder="Author" className="input"/>
+      <button type="submit" className="form-button">Add Book</button>
+    </form>
+  </div>
   );
   export default Books;
+
+  const Book = ({ title, author }) => (
+    <div className="book">
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <button type="button" className="remove-button">Remove </button>
+    </div>
+  );
+  
+  Book.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
+import './styles/Navbar.css'
 
 const Navbar = () => (
   <nav className="nav">
