739d30eff74f36ba62fa84e48f4015dc8bba76a6
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-04-05 06:32:21 +0300

    create event for categories section

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './styles/Categories.css';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <div>
-    <button className="cat-btn" type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories.status);
+  return (
+    <div>
+      <h1>{status}</h1>
+      <button className="cat-btn" type="button" onClick={() => dispatch(checkStatus())}>
+        Check status
+      </button>
+    </div>
+  );
+};
 export default Categories;
